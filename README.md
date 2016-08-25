d84f9c52fef5aab3a324118eccafeab62aa0cc93
Chad Griffis <cmg123@me.com>
2016-08-25 12:44:45 -0500

    Adds packkage.json with dependencies, gulpfile shell, index.js shell, updates gitignore and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Co-Star
+
+A web application for finding movies that your favorite movie stars have co-stared in.
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task(default, function(){
+
+  //place code for you default taks here
+
+});
