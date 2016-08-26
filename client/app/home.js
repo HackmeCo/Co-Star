b98f83a89d29ce451d8fdc37ff4397d10bb14980
KathrynmHansen <katie112892@aol.com>
2016-08-26 11:53:35 -0500

    promises should be fixed

--- client/app/home.js
+++ client/app/home.js
@@ -3,7 +3,7 @@ angular.module('costars.home' , [])
 
 //THE CONTROLLER FOR THE ENTIRE COSTARS WEBSITE
 
-.controller('HomeController', function($scope, $location, $http, ApiCalls)) {
+.controller('HomeController', function($scope, $location, $http, ApiCalls, DB)) {
   //$scope.data = {}; will need for movies
   $scope.currentSearches = []; //array of actors with associated ids. 
   $scope.actorIds = []; //it will be a mapping of actor names to ids.
@@ -13,26 +13,58 @@ angular.module('costars.home' , [])
   $scope.getMovies = function (){
     if($scope.currentSearches.length === 1){
       //api call for one persons stuff
-      ApiCalls.searchByPerson($scope.currentSearches[0]); // maybe .then( display stuff)
+      ApiCalls.searchByPerson($scope.currentSearches[0]) // maybe .then( display stuff)\
+        .then(function(data){
+          //stores all the information given in the database
+          DB.storeActor(data)
+          .then(function(resp){
+            //must .then a promise.
+            console.log("actor information stored");
+            console.log('resp', resp);
+          })
+        }) 
     }
     else{
+      //need a promise array so we can .then the for loop
       var promise = [];
+      //loop through all the current searches
       for( let i = 0; i < currentSearches.length; i++){
-        
-        promise.push(getActor(currentSearches[i])
-        .then(function(data){
-          $scope.ids.push(data.id);
-        })
-        .catch(function(){
-          $scope.ids.push(ApiCalls.searchByPerson($scope.currentSearches[i]));
-        }));
-        //currently i'm assumming that searchbyPerson is going to return an actors id.
+        //get your actor information from the database
+        promise.push(DB.getActor(currentSearches[i])
+          .then(function(data){
+            $scope.ids.push(data.id);
+          })
+          //if we dont get any data back, api call time
+          .catch(function(){
+            ApiCalls.searchByPerson($scope.currentSearches[i])
+              .then(function(data){
+              //assuming that data.id is id. DOUBLE CHECK THAT!!
+                var id = data.id
+                $scope.ids.push(id)
+                //we need to store the new information in the database
+                DB.storeActor(data)
+                  .then(function(){
+                    //it worked
+                    console.log("data stored")
+                  })
+                  .catch(function(err){
+                    //it failed
+                    console.log("we have an error", err);
+                  })
+              })
+          })  
+        );
       }
-      Promise.all(promise).then(function(){
-
-      ApiCalls.discover($scope.actorIds);
-        
-      });
+      //promise.all your promises.
+      Promise.all(promise)
+        .then(function(){
+          //make discover api call finally
+          ApiCalls.discover($scope.actorIds)
+            .then(function(data){
+              //eventually we need to show the data here!!!
+              console.log("DATA FROM DISCOVER CALL", data);
+            });
+        });
     }
     
     
