12ba0aa6f242d39b92d602f9920ce25d1ec14b1f
Chad Griffis <cmg123@me.com>
2016-08-26 10:07:15 -0500

    adds helpers, db, and endfiles for server structure (#22)

---
+++ server/db.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var pmongo = require('promised-mongo');
+var mongodb = require('mongodb');
+
+//needs db to be defined with our mongodb path
+module.exports = db;
---
+++ server/endpoints.js
@@ -0,0 +1,3 @@
+// GET /thespians
+
+// POST /thespians
