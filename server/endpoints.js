a45af79539051409388f71c2254d440492ceb6c3
GMDIV <mitchelld20@yahoo.com>
2016-08-26 11:15:30 -0500

    added mongodb paths; express static capabilities

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,3 +1,15 @@
+
+
+
 // GET /thespians
 
+app.get('/thespians', function (req, res) {
+  res.sendFile( path.join(__dirname, '../client', 'index.html') );
+})
+
+
 // POST /thespians
+
+app.post('/thespians', function (req, res) {
+  res.sendFile( path.join(__dirname, '../client', 'index.html') );
+});
