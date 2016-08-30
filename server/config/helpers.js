4368c6faade8a83555747bcec346f2bec2dbbc2b
Chad Griffis <cmg123@me.com>
2016-08-30 19:08:22 -0400

    refactors db to use mongoose, updates package.json, removes server/db.js and endpoins.js, adds server/model/db.js and thespian.js, updates index.js (#68)

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -4,5 +4,8 @@ module.exports = {
     // message for gracefull error handling on app
     res.status(500).send({error: error.message});
   },
-
+  notInDb: function (result, req, res, next) {
+    //send 404 message to client when item not in db
+    res.status(404).send("Thespian not in local DB.");
+  }
 };
