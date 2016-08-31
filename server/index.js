c75d7cb4003923acfa889ed050a9b1b537838e44
Chad Griffis <cmg123@me.com>
2016-08-31 15:08:14 -0400

    adds dropDups to id in thespianSchema ensuring no duplicate docs in db, also adds comments and refactors a few console.logs (#73)

--- server/index.js
+++ server/index.js
@@ -25,13 +25,19 @@ app.use('/bower_components', express.static(path.join(__dirname, '../bower_compo
 //              thespians routes
 //===============================================
 
+    /*
+    get to /thespians will take the name of an actor (this name comes in as a query, ex: /thespains?name=Tom+Hanks)
+    and then do a search through our database looking for the name of that actor and provide back the full actor's
+    object with all of the information on that actor that has been stored
+    */
+
 app.get('/thespians', function (req, res) {
   var thespian = req.query.name;
   Thespian.find({ name : thespian }).exec(function(err, result) {
     if(err) {
       return helpers.errorHandler(err, req, res);
     } else {
-      console.log('this is the returned item from Thespian.find: ', result);
+      //console.log('this is the returned item from Thespian.find: ', result);
       if (result.length > 1) {
         //if there is more than one thespian by the same name, this will send an array
         //with the object for each thespian
@@ -49,16 +55,22 @@ app.get('/thespians', function (req, res) {
 
 });
 
-app.post('/thespians', function (req, res, next) {
+    /*
+    post to /thespians takes the thespian object that is sent and then inserts it into our 
+    thespians table. This will be parsed out of the movieDB api get request data into a more consolidated 
+    form of information that we will use. 
+    */
+
+app.post('/thespians', function (req, res) {
   var thespianObj = req.body.data;
-  console.log('The post to thespians getting triggered, here is the thespianObj: ', thespianObj);
+  console.log('The post to thespians getting triggered');
   var NewThespian = new Thespian (thespianObj);
   NewThespian.save(function (err, post) {
     if (err) {
-      console.error('Error in index.js line 58: ', err);
+      console.error('Error in index.js line 70: ', err);
       return helpers.errorHandler(err, req, res);
     }
-    console.log('NewThespian saved and it is a good day: ', post);
+    console.log('NewThespian has entered our DB');
     res.send(post);
   })
 });
--- server/model/thespian.js
+++ server/model/thespian.js
@@ -2,7 +2,9 @@ var mongoose = require( 'mongoose' );
 var co = require('co');
 
 var thespianSchema = new mongoose.Schema({
-  id          : { type: Number, required: true, unique: true },
+  // dropDups: true forces the creation of a unique index on the collection.  This will force
+  // MongoDB to create a unique index by deleting documents with duplicate values
+  id          : { type: Number, required: true, unique: true, dropDups: true }, 
   known_for   : Array,
   name        : { type: String, required: true },
   popularity  : Number,
