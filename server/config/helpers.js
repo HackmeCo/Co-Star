43e277821c5c66628a890fb0985f62931cea4707
GMDIV <mitchelld20@yahoo.com>
2016-09-02 13:55:50 -0500

    added leaderboards database and endpoints

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -8,4 +8,7 @@ module.exports = {
     //send 404 message to client when item not in db
     res.status(404).send("Thespian not in local DB.");
   }
+  noHighScoresYet: function(result, req, res, next) {
+  	res.status(404).send("There are no high scores yet in the Leaderboards database");
+  }
 };
