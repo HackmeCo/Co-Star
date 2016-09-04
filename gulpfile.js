7ebc43c35f2b049dee779514770abe8c8e14ec3f
Chad Griffis <cmg123@me.com>
2016-09-04 19:12:30 -0400

    adds gulpfile and package.json update

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,10 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
-gulp
-  .src([
-  './server/**/*.js',
-  './client/**/*.js',
-  ])
+gulp.task('jshint', function(){
+  gulp
+  .src(['./server/**/*.js','./client/**/*.js',])
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'))
   .pipe(jshint.reporter('fail'));
-
-gulp.task('jshint', function(){
-
 });
