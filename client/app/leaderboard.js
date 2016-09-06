66f3e10ce54c8a03c1020021d73d45dc3c238bc6
Elliot Cheung <ezcheung>
2016-09-06 10:08:27 -0500

    Changed some comments

--- client/app/leaderboard.js
+++ client/app/leaderboard.js
@@ -1,15 +1,20 @@
 angular.module('costars.leaderboard', [])
 
 .controller('LeaderboardController', function($scope, Leaderboard, $location){
+  /*
+  * Functions for redirecting to other pages
+  */
+
   $scope.goToGame = function(){
     $location.path("/game");
   };
   $scope.goToHome = function(){
     $location.path("/");
   };
-  $scope.scores = [];
-  $scope.loaded = false;
-  $scope.error = false;
+  
+  $scope.scores = []; //The scores, as objects {name: String, score: Number}
+  $scope.loaded = false; //Set to true when the leaderboard has loaded
+  $scope.error = false; //Displays an error message if the scores fail to load
 
   Leaderboard.getScores() //fetch high scores
   .then(function(data){
